425ddf52515caed1107ab7e21abfba85cb03aec0
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-13 19:39:40 -0300

    Server Side Rendering

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar/navbar";
+
 
 const About = () => {
-    return  (
-       <div>
-          <h1>
-            This is the About Page !
-        </h1>
-        
-       </div> 
-      
-    );
+  return (
+    <div>
+      <Navbar />
+        <p> This is the About Page !</p>
+    </div>
+  );
 };
 
 export default About
